e34decc6ad6f8adaaa3e3a5987fe98195205e4ca
hedaiyong <hedy02@vanke.net.cn>
2017-01-14 21:23:58 +0800

    default

--- html/sign/ctrl.js
+++ html/sign/ctrl.js
@@ -2,7 +2,7 @@
  * Created by hedy02 on 2016/9/19.
  */
 'use strict';
-signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $http, AUTH_EVENTS, $rootScope,AuthService, GLOBAL_CONFIG,Session,Md5) {
+signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $http, AUTH_EVENTS, $rootScope,AuthService, GLOBAL_CONFIG,Session,Md5,GLOBAL_VALUE ) {
     
     $scope.credentials = {
         nickname:'',
@@ -64,10 +64,10 @@ signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $
                         $scope.common.tvercode.showError = false;
                         $scope.common.vercode.text = '验证码已发送';
                         $scope.common.vercode.disabled = true;
-                    }else if(ret.code = '107'){
+                    }else if(ret.code == '107'){
                         $scope.common.phone.showError = true;
                         $scope.common.phone.msg = "手机号格式不正确哦";
-                    }else if(ret.code = '108'){
+                    }else if(ret.code == '108'){
                         $scope.common.tvercode.showError = true;
                         $scope.common.tvercode.msg = "验证码不对哦";
                     }
@@ -104,8 +104,8 @@ signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $
                     $scope.common.vercode.showError = true;
                     $scope.common.vercode.msg = "验证码不对哦";
                 }else if (res.data.code == '111'){
-                    $scope.common.vercode.showError = true;
-                    $scope.common.vercode.msg = "手机号不一致";
+                    $scope.common.phone.showError = true;
+                    $scope.common.phone.msg = "手机号不一致";
                 }
 
             });
