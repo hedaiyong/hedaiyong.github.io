0316b5c4954d7eaf425a44356cbea168bd57fba2
hedaiyong <hedy02@vanke.net.cn>
2017-01-14 14:19:44 +0800

    default

--- html/sign/ctrl.js
+++ html/sign/ctrl.js
@@ -2,7 +2,7 @@
  * Created by hedy02 on 2016/9/19.
  */
 'use strict';
-signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $rootScope, AUTH_EVENTS, AuthService, GLOBAL_CONFIG) {
+signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $http, AUTH_EVENTS, $rootScope,AuthService, GLOBAL_CONFIG) {
     
     $scope.credentials = {
         nickname:'',
@@ -23,6 +23,7 @@ signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $
         },
         //短信验证码
         vercode:{
+            disabled:false,
             showError:false,
             text:'发送验证码',
             msg:''
@@ -41,22 +42,34 @@ signUp_project_app.controller('signUp_project_ctrl', function ($window,$scope, $
     //发送手机验证码
     $scope.common.sendCode = function () {
 
-        if ($scope.credentials.phone == '' || $scope.credentials.phone == null || $scope.credentials.tvercode.phone != 11){
+        if ($scope.credentials.phone == '' || $scope.credentials.phone == null ||  $scope.credentials.phone.length != 11){
             $scope.common.phone.showError = true;
-            $scope.common.phone.msg = "手机号格式不正确哦";
+            if ( $scope.credentials.phone.length != 11)
+                $scope.common.phone.msg = "手机号格式不正确哦";
+            else $scope.common.phone.msg = "手机号没有输入哦";
+
         }else if ($scope.credentials.tvercode == '' || $scope.credentials.tvercode == null || $scope.credentials.tvercode.length != 4){
             $scope.common.tvercode.showError = true;
-            $scope.common.tvercode.msg = "验证要输入哦";
+            if ($scope.credentials.tvercode.length != 4)
+                $scope.common.tvercode.msg = "验证码格式不对哦";
+            else   $scope.common.tvercode.msg = "验证码没有输入哦";
         }else {
             $http.get('http://' + GLOBAL_CONFIG.url.ip + ':' + GLOBAL_CONFIG.url.port + '/api/sendVercodeOpen?'
                 + '&phone=' + $scope.credentials.phone
                 + '&tvercode=' + $scope.credentials.tvercode
-                + '&key=' + key)
+                )
                 .success(function (ret) {
                     if (ret.code == '000') {
                         $scope.common.phone.showError = false;
                         $scope.common.tvercode.showError = false;
                         $scope.common.vercode.text = '验证码已发送';
+                        $scope.common.vercode.disabled = true;
+                    }else if(ret.code = '107'){
+                        $scope.common.phone.showError = true;
+                        $scope.common.phone.msg = "手机号格式不正确哦";
+                    }else if(ret.code = '108'){
+                        $scope.common.tvercode.showError = true;
+                        $scope.common.tvercode.msg = "验证码不对哦";
                     }
                 }).error(function (msg) {
                 console.log("Fail! " + msg);
